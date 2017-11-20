0a419dbd585de63a49fa1c261855ea7eef186b77
VuongVu <vuongvu333@gmail.com>
2017-11-20 22:29:15 +0700

    Change to use ngx-bootstrap/bootstrap4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-import {
-  Router,
-  Event as RouterEvent,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-} from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 
 @Component({
@@ -15,13 +7,7 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isLoading = false;
-
-  constructor(private router: Router, private meta: Meta) {
-    router.events.subscribe((event: RouterEvent) => {
-      this.navigationInterceptor(event);
-    });
-
+  constructor(private meta: Meta) {
     meta.addTags([
       { name: 'author', content: 'Vuong Vu' },
       { name: 'keywords', content: 'angular awesome, angular 4' },
@@ -30,22 +16,5 @@ export class AppComponent {
         content: 'Angular Awesome',
       },
     ]);
-  }
-
-  // Shows and hides the loading spinner during RouterEvent changes
-  navigationInterceptor(event: RouterEvent): void {
-    if (event instanceof NavigationStart) {
-      this.isLoading = true;
-    }
-    if (event instanceof NavigationEnd) {
-      this.isLoading = false;
-    }
-    // Set loading state to false in both of the below events to hide the spinner in case a request fails
-    if (event instanceof NavigationCancel) {
-      this.isLoading = false;
-    }
-    if (event instanceof NavigationError) {
-      this.isLoading = false;
-    }
   }
 }
